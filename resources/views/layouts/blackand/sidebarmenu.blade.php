75c0f2c92ae526d0f4d51ee21facbc7cd6957dc9
fauzi ahmad <ozanfauzi39@gmail.com>
2023-06-19 22:48:14 +0700

    user

--- resources/views/layouts/blackand/sidebarmenu.blade.php
+++ resources/views/layouts/blackand/sidebarmenu.blade.php
@@ -24,15 +24,11 @@
         <a href="/profil" class="d-block">{{ Auth::user()->name }}</a>
       </div>
     </div>
-
-
-
     <!-- Sidebar Menu -->
     <nav class="mt-2">
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         <!-- Add icons to the links using the .nav-icon class
              with font-awesome or any other icon font library -->
-
         <li class="nav-item">
           <a href="{{ route('dashboard') }}" class="nav-link">
             <i class="nav-icon fas fa-home"></i>
