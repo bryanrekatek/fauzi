6365ea88d1decd84e30aa768de5f8e26126c9143
fauzi ahmad <ozanfauzi39@gmail.com>
2023-06-19 22:56:45 +0700

    user

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -5,12 +5,12 @@
     <div class="container-fluid">
       <div class="row mb-2">
         <div class="col-sm-6">
-          <h1 class="m-0">Data Permission</h1>
+          <h1 class="m-0">Data Pengguna</h1>
         </div><!-- /.col -->
         <div class="col-sm-6">
           <ol class="breadcrumb float-sm-right">
             <li class="breadcrumb-item"><a href="#">Beranda</a></li>
-            <li class="breadcrumb-item active">Data Permission</li>
+            <li class="breadcrumb-item active">Data Pengguna</li>
           </ol>
         </div><!-- /.col -->
       </div><!-- /.row -->
